679bf97caf176289805652e6909f13e4b6f5d5c1
zulfan2882 <zulfan2882.mza@gmail.com>
2021-11-03 05:28:57 +0700

    python.lingkaran

---
+++ lingkaran.py
@@ -0,0 +1,11 @@
+print("program untuk mencari luas dan keliling lingkaran")
+
+import math
+r=float(input("masukkan jari-jari : "))
+
+phi=22/7
+luas=math.phi*(r*r)
+keliling=2*math.phi*r
+
+print("luas lingkaran \t\t= ",luas)
+print("keliling lingkaran\t= ",keliling)
